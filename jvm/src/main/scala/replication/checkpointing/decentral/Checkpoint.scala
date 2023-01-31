a63a4fb0c99e6aec95f91394915156fd5f10ced3
ragnar <git@rmgk.de>
2023-01-31 10:00:12 +0100

    rove unrelated example code

--- jvm/src/main/scala/replication/checkpointing/decentral/Checkpoint.scala
+++
@@ -1,4 +0,0 @@
-package replication.checkpointing.decentral
-import kofre.base.Id
-
-case class Checkpoint(replicaID: Id, counter: Int)
--- jvm/src/main/scala/replication/cli.scala
+++ jvm/src/main/scala/replication/cli.scala
@@ -1,10 +1,6 @@
 package replication
 
 import de.rmgk.options.*
-import replication.calendar.{CalendarOptions, Peer}
-import replication.checkpointing.CheckpointingOptions
-import replication.checkpointing.central.{CentralOptions, Checkpointer}
-import replication.checkpointing.decentral.{DecentralOptions, Replica}
 import kofre.base.Id
 import replication.fbdc.FbdcCli
 
@@ -12,9 +8,6 @@ import java.nio.file.Path
 
 
 case class CliArgs(
-    calendar: Subcommand[CalendarOptions] = Subcommand(CalendarOptions()),
-    dtn: Subcommand[Unit] = Subcommand.empty(),
-    checkpointing: Subcommand[CheckpointingOptions] = Subcommand(CheckpointingOptions()),
     conn: Subcommand[fbdc.CliConnections] = Subcommand(fbdc.CliConnections()),
 )
 
@@ -35,51 +28,6 @@ object cli {
     scopt.OParser.parse(parser, args, instance)
 
     val ipAndPort = """([\d.]*):(\d*)""".r
-
-    instance.calendar.value match
-      case None =>
-      case Some(calArgs) =>
-        val ipsAndPorts = calArgs.connectTo.value.collect {
-          case ipAndPort(ip, port) => (ip, port.toInt)
-        }
-
-        new Peer(Id.predefined(calArgs.id.value), calArgs.listenPort.value, ipsAndPorts).run()
-
-    instance.checkpointing.value.flatMap(_.decentral.value) match
-      case None =>
-      case Some(decArgs) =>
-        val ipsAndPorts = decArgs.connectTo.value.collect {
-          case ipAndPort(ip, port) => (ip, port.toInt)
-        }
-
-        new Replica(
-          decArgs.listenPort.value,
-          ipsAndPorts,
-          Id.predefined(decArgs.id.value),
-          decArgs.initSize.value
-        ).run()
-
-    instance.checkpointing.value.flatMap(_.central.value) match
-      case None =>
-      case Some(centralArgs) =>
-        centralArgs.peer.value match
-          case None =>
-          case Some(centralPeerArgs) =>
-            val ipsAndPorts = centralPeerArgs.connectTo.value.collect {
-              case ipAndPort(ip, port) => (ip, port.toInt)
-            }
-
-            new replication.checkpointing.central.Peer(
-              Id.predefined(centralPeerArgs.id.value),
-              centralPeerArgs.listenPort.value,
-              ipsAndPorts
-            ).run()
-        centralArgs.checkpointer.value match
-          case None =>
-          case Some(checkpointerArgs) =>
-            new Checkpointer(checkpointerArgs.listenPort.value).run()
-
-    if instance.dtn.value.isDefined then dtn.run()
 
     instance.conn.value match
       case None =>
