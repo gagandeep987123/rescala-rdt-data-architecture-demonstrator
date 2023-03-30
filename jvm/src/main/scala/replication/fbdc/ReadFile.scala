e1ce2628db4d98c3d166489617c8a785ddc22944
Gagandeep <gagandeep@pc-indy14.st.informatik.tu-darmstadt.de>
2023-03-30 16:52:33 +0200

    some changes

---
+++ jvm/src/main/scala/replication/fbdc/ReadFile.scala
@@ -0,0 +1,40 @@
+package replication.fbdc
+
+import de.rmgk.script.extensions
+import kofre.datatypes.LastWriterWins
+
+import java.nio.file.{Files, Paths}
+
+object ReadFile {
+
+  def enableConditional(exampleData: FbdcExampleData) = {
+    // adapt for other requirements
+    if Files.exists(Paths.get("hello.py")) &&
+      Files.exists(Paths.get("readFile.py")) &&
+      process"which python3".runResult().isRight
+    then
+      println(s"enabling Read Box")
+      enableReadFileProcessing(exampleData)
+    else
+      println(s"python or hello.py not installed")
+  }
+
+  def enableReadFileProcessing(exampleData: FbdcExampleData) =
+    import exampleData.dataManager
+    exampleData.addCapability("ReadFile")
+
+    exampleData.requestsOf[Req.ReadFile].observe { ReadFile =>
+      dataManager.transform { current =>
+        current.modRes ( reqq =>
+            ReadFile.foreach { q =>
+            val resp = processReadFile(q.value)
+            reqq.observeRemoveMap.insert("ReadFile", Some(LastWriterWins.now(resp, exampleData.replicaId)))
+          }
+        )
+      }
+    }
+
+  def processReadFile(r: Req.ReadFile) =
+    Res.ReadFile(r, Files.readString(Paths.get("results")))
+
+}
